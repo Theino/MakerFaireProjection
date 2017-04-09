2cf49b90c73f7a47256a64b13ae34587e871047c
Theino <theino8@yahoo.com>
2017-04-09 16:37:14 -0400

    more csv loading

--- MakerFaireProjection/Form1.cs
+++ MakerFaireProjection/Form1.cs
@@ -6,20 +6,21 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;'
+using System.Windows.Forms;
 using System.IO;
 
 namespace MakerFaireProjection
 {
     public struct SensorData
     {
-        DateTime dateTime;
-        float value;
+        public DateTime dateTime;
+        public float value;
     }
     public partial class Form1 : Form
     {
         StreamReader reader;
         string currentLine;
+        DateTime currFakeTime;
         public Form1()
         {
             InitializeComponent();
@@ -31,10 +32,8 @@ namespace MakerFaireProjection
             if (reader == null)
                 throw new Exception("Couldn't load csv");
             RemoveDescriptionLine();
-            currentLine = ReadCSVLine(currentLine);
-            string[] dataSplitString = currentLine.Split(',');
-            string[] dateSplit = dataSplitString[0].Split('/');
-            string[] timeSplit = dataSplitString[1].Split(':');
+            SensorData firstData = PopulateSensorData(reader.ReadLine());
+            currFakeTime = firstData.dateTime;
         }
 
         private string ReadCSVLine(string oldLine)
@@ -53,6 +52,28 @@ namespace MakerFaireProjection
         private void RemoveDescriptionLine()
         {
             reader.ReadLine();
+        }
+
+        private SensorData PopulateSensorData(string line)
+        {
+            string[] dataSplitString = line.Split(',');
+            string[] dateSplit = dataSplitString[0].Split('/');
+            string[] timeSplit = dataSplitString[1].Split(':');
+            DateTime currDateTime = new DateTime(int.Parse(dateSplit[2]), int.Parse(dateSplit[1]), int.Parse(dateSplit[0]), int.Parse(timeSplit[0]), int.Parse(timeSplit[1]), int.Parse(timeSplit[2]));
+            SensorData currSensorData;
+            currSensorData.dateTime = currDateTime;
+            currSensorData.value = float.Parse(dataSplitString[2]);
+            return currSensorData;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            currentLine = ReadCSVLine(currentLine);
+            if (currentLine != null)
+            {
+                SensorData sensorData = PopulateSensorData(currentLine);
+
+            }
         }
     }
 }
