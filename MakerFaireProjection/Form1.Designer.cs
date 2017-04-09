78577a5c7e2853b329f20767c0588b302fb8be40
Theino <theino8@yahoo.com>
2017-04-09 16:17:10 -0400

    Start of csv parsing

--- MakerFaireProjection/Form1.Designer.cs
+++ MakerFaireProjection/Form1.Designer.cs
@@ -29,11 +29,42 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtBoxErrors = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtBoxErrors
+            // 
+            this.txtBoxErrors.AllowDrop = true;
+            this.txtBoxErrors.Location = new System.Drawing.Point(242, 26);
+            this.txtBoxErrors.Name = "txtBoxErrors";
+            this.txtBoxErrors.ReadOnly = true;
+            this.txtBoxErrors.Size = new System.Drawing.Size(413, 22);
+            this.txtBoxErrors.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 506);
+            this.Controls.Add(this.txtBoxErrors);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtBoxErrors;
     }
 }
 
--- MakerFaireProjection/Form1.cs
+++ MakerFaireProjection/Form1.cs
@@ -6,15 +6,53 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Windows.Forms;'
+using System.IO;
 
 namespace MakerFaireProjection
 {
+    public struct SensorData
+    {
+        DateTime dateTime;
+        float value;
+    }
     public partial class Form1 : Form
     {
+        StreamReader reader;
+        string currentLine;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            reader = new StreamReader("airmote.csv");
+            if (reader == null)
+                throw new Exception("Couldn't load csv");
+            RemoveDescriptionLine();
+            currentLine = ReadCSVLine(currentLine);
+            string[] dataSplitString = currentLine.Split(',');
+            string[] dateSplit = dataSplitString[0].Split('/');
+            string[] timeSplit = dataSplitString[1].Split(':');
+        }
+
+        private string ReadCSVLine(string oldLine)
+        {
+            if (oldLine == "")
+            {
+                oldLine = reader.ReadLine();
+                if (oldLine == null)
+                {
+                    throw new Exception("ERROR: Unable to read line in CSV");
+                }
+            }
+            return oldLine;
+        }
+
+        private void RemoveDescriptionLine()
+        {
+            reader.ReadLine();
         }
     }
 }
